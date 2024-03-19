e3bfcfff992156f0122637520a9c6d9955ec69d2
Xiyu-Serena-Wan <64742214+Xiyu-Serena-Wan@users.noreply.github.com>
2024-03-19 11:34:38 -0700

    made the board

---
+++ gameoflife/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Game from './Game';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Game Of Life</h1>
+      <Game/>
+    </div>
+  );
+}
+
+export default App;
